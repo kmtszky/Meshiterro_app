be02649ff12f8792d5f9ae99ef2da83924d3c8cf
kmtszky <kmtszk711@gmail.com>
2021-03-08 12:08:09 +0000

    [Update] 4-3: post_images_controller > 定義をupdate

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,18 +1,30 @@
 class PostImagesController < ApplicationController
 
   def new
+    @post_image = PostImage.new
   end
 
   def create
+    @post_image = Postimage.new(post_image_params)
+    @post_image.user_id = ourrent_user.id
+    @post_image.save
+    redirect_to post_image_path
   end
 
   def index
+    @post_image = PostImage.all
   end
 
   def show
   end
 
   def desroy
+  end
+
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
   end
 
 end
