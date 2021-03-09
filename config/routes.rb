0f8a053685ddea102b62e5bdf0e74bc5ce48541d
kmtszky <kmtszk711@gmail.com>
2021-03-09 03:24:07 +0000

    [update] 4-4: routes.rb > post_commentsに対してresources設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users     # deviseを使用する際にURLとしてusersを含む
   root to: 'homes#top'  # topページをルートパスにする
 
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  #コメントは、投稿画像に対してコメントされるため、post_commentsは、post_imagesに結びつく ⇒ 親子関係になる
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
 
 end
