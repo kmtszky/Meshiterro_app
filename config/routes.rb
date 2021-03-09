76a08bd2b23305bd729d61378faa93697f104dc8
kmtszky <kmtszk711@gmail.com>
2021-03-09 08:27:36 +0000

    [Add] 4-4: favorite model追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   #コメントは、投稿画像に対してコメントされるため、post_commentsは、post_imagesに結びつく ⇒ 親子関係になる
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
 
